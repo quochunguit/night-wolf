cf6000efd882a3ecc66ef11f3d8bbc3bc1af29f4
quochung2018 <hung@bizzon.vn>
2021-12-02 10:14:48 +0700

    aaa

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+
+
+$(".page-header .top-header .md-toggle-menu .md-open").click(function(){
+  $(".page-header .top-header .main-menu-mb").addClass('active');
+});
+
+$(".page-header .top-header .md-close").click(function(){
+  $(".page-header .top-header .main-menu-mb").removeClass('active');
+});
+
+$('.md-slide .owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+var height_1 = $('.page-footer .md-contact .md-content .md-row-1').outerHeight();
+$('.page-footer .md-contact .md-content .md-row-2').css('height',height_1);
+
+new WOW().init();
+// $("#md-tk").fancybox().trigger('click');
+
